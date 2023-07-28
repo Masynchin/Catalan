81752d63c38d7d4a9e52f438cdff6ac3958bd836
Max Smirnov <masynchin@gmail.com>
2023-07-28 12:39:16 +0300

    Add algorithm

--- main.hs
+++ main.hs
@@ -1,2 +1,17 @@
 main :: IO ()
-main = putStrLn "Hello, Catalan!"
+main = putStrLn . show . take 10 $ coeffs
+
+accumulate :: [Int] -> [Int]
+accumulate = scanl1 (+)
+
+firstColumn :: [Int]
+firstColumn = repeat 1
+
+nextColumn :: [Int] -> [Int]
+nextColumn = accumulate
+
+simulate :: [Int] -> [Int]
+simulate (x:xs) = x : simulate (nextColumn xs)
+
+coeffs :: [Int]
+coeffs = simulate firstColumn
